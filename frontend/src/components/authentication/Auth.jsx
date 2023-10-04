033b3e8deda163d2e6602bf6bbafab351f5ddc78
Utkarsh Kadam <99534647+Utkarsh-kadam@users.noreply.github.com>
2023-10-04 18:34:34 +0530

    build

--- frontend/src/components/authentication/Auth.jsx
+++ frontend/src/components/authentication/Auth.jsx
@@ -36,7 +36,7 @@ function Login() {
 
 
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://checkers-backend.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
